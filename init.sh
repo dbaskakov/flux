e0de6dfae45ffd0a42d2a23879977c97539af8e5
Dmitry Baskakov <d.baskakov@softpro.com>
2020-08-27 15:41:33 +0300

    up version

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+helm upgrade -i flux fluxcd/flux \
+   --set git.url=git@github.com:dbaskakov/flux \
+   --set syncGarbageCollection.enabled=true \
+   --namespace flux
+
+fluxctl identity --k8s-fwd-ns flux
+
 helm upgrade -i helm-operator fluxcd/helm-operator --namespace flux \
 --set helm.versions=v3 \
 --set configureRepositories.enable=true \
